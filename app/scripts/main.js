a37f76f0f67ade3660c868cc93b4500e1c2fa0f4
Matt Hartley <mbhartley1490@gmail.com>
2014-01-27 14:56:54 -0500

    first constructor built

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+function FamilyMember(firstName){
+	this.firstName = firstName;
+	this.lastName = 'Hartley';
+	this.birthPlace = 'Dallas, TX';
+}
+
+var mike = new FamilyMember ('Mike') 
+var charlotte = new FamilyMember ('Charlotte')
+var brian = new FamilyMember ('Brian')
+
+console.log('I created a simple constructor called FamilyMember. Type FamilyMember')
+console.log('My var names are "mike" "charlotte" and "brian" Type each.')
+
